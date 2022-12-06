34b55a287c04826791dee932e69ad78578ec88cb
dongE <68381747+Daae-Kim@users.noreply.github.com>
2022-12-06 21:02:24 +0900

    [Bronze V] Title: A+B - 2, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        System.out.println(n+m);
+    }
+}
