35075382bf9327c96fa5a800275a096bfef89c7c
dongE <68381747+Daae-Kim@users.noreply.github.com>
2022-12-08 11:40:58 +0900

    [Bronze IV] Title: 기찍 N, Time: 920 ms, Memory: 30692 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for(int i=N; i>0; i--){
+            System.out.println(i);
+        }
+    }
+}
